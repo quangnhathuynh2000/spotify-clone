af580a56bc5bf86889af9a9c3f60ec01de44ef93
quangnhathuynh2000 <huynhnhatquang101220@gmail.com>
2023-02-22 18:27:13 +0700

    v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+/* eslint-disable jsx-a11y/heading-has-content */
+import React from 'react'
 import './App.css';
+import Player from './Player'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App" >
+
+      <Player />
+
+    </div >
   );
 }
 
